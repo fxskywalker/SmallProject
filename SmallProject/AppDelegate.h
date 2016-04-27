22c8d7421398c0210efa62de0e9fd1dd4c7a5ed7
Charlie Fang <xf39@njit.edu>
2016-04-27 12:24:22 -0500

    Initial Commit

---
+++ SmallProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmallProject
+//
+//  Created by FangXin on 4/27/16.
+//  Copyright © 2016 FangXin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmallProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmallProject
+//
+//  Created by FangXin on 4/27/16.
+//  Copyright © 2016 FangXin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
