7d9dfad6941c6dabdd88b7d0cf568af7a776b56a
Charlie Fang <xf39@njit.edu>
2016-04-28 14:25:45 -0500

    git more data

--- SmallProject/MainViewController.m
+++ SmallProject/MainViewController.m
@@ -28,14 +28,7 @@ NSString *accessToken = @"ee544bba51cf4b4cbf1e1391443cb0c1";
   // Do any additional setup after loading the view, typically from a nib.
   APIManager * temp = [APIManager sharedInstance];
   [temp getVedioAndImageLinkArray];
-  [temp getImageByLink:^(NSURL *reply){
-    MPMoviePlayerController *moviePlayerController = [[MPMoviePlayerController alloc] initWithContentURL:reply];
-    [moviePlayerController.view setFrame:self.view.frame];
-    [self.view addSubview:moviePlayerController.view];
-    moviePlayerController.movieSourceType=MPMovieSourceTypeFile;
-    moviePlayerController.fullscreen = YES;
-    [moviePlayerController play];
-  }];
+  
  
 }
 
