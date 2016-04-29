a1dda2dc3fc061dd9d4a699109606576db9218b9
Charlie Fang <xf39@njit.edu>
2016-04-29 00:04:30 -0500

    finished preview video cell

---
+++ SmallProject/LargeVideoViewController.m
@@ -0,0 +1,13 @@
+//
+//  LargeVideoViewController.m
+//  SmallProject
+//
+//  Created by FangXin on 4/29/16.
+//  Copyright © 2016 FangXin. All rights reserved.
+//
+
+#import "LargeVideoViewController.h"
+
+@implementation LargeVideoViewController
+
+@end
